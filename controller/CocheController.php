d43e78acc681eb074981b54b7bfef53a39aa9ff8
RibeiroLucas <lucasribeiro1024b@gmail.com>
2022-02-23 11:25:33 -0300

    Discovered problem while handling urls.

--- controller/CocheController.php
+++ controller/CocheController.php
@@ -2,23 +2,35 @@
 
 class CocheController
 {
-    var $coches;
+  var $coches;
 
-    function __construct()
-    {
-        $this->coches = [
-            1 => new Coche("Wolkswagen","Polo","negro","Rebeca"),
-            2 => new Coche("Toyota","Corolla","verde","Marcos"),
-            3 => new Coche("Skoda","Octavia","gris","Mario"),
-            4 => new Coche("Kia","Niro","azul","Jairo")
-        ];
+  function __construct()
+  {
+    $this->coches = [
+      1 => new Coche("Wolkswagen","Polo","negro","Rebeca"),
+      2 => new Coche("Toyota","Corolla","verde","Marcos"),
+      3 => new Coche("Skoda","Octavia","gris","Mario"),
+      4 => new Coche("Kia","Niro","azul","Jairo")
+    ];
+  }
+
+  public function index(){
+    //Asigno los coches a una variable que estará esperando la vista
+    $rowset = $this->coches;
+
+    //Le paso los datos a la vista
+    require("view/index.php");
+  }
+
+  public function ver($iId){
+    if (array_key_exists($iId, $this->coches)) {
+      //Si el elemento está en el array, lo muestro
+      $row = $this->coches[$iId];
+      require("view/ver.php");
     }
-
-    public function index(){
-        //Asigno los coches a una variable que estará esperando la vista
-        $rowset = $this->coches;
-
-        //Le paso los datos a la vista
-        require("view/index.php");
+    else {
+      //Llamo al método por defecto del controlador
+      $this->index();
     }
+  }
 }
---
+++ view/ver.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<style>
+    th{
+        width: 8rem;
+        text-align: left;
+        border-bottom: 1px solid black;
+    }
+    td{
+        width: 8rem;
+    }
+</style>
+
+<h1>Ejemplo 6: Vista de coche</h1>
+<table>
+    <tr>
+        <th>Marca</th>
+        <th>Modelo</th>
+        <th>Color</th>
+        <th>Propietario</th>
+    </tr>
+
+    <tr>
+        <td><?php echo $row->marca ?></td>
+        <td><?php echo $row->modelo ?></td>
+        <td><?php echo $row->color ?></td>
+        <d><?php echo $row->propietario ?></td>
+    </tr>
+</table>
+</body>
+</html>
